c506a5216160968686b14ef7b39b85241382d343
Frazzer951 <luke343279@gmail.com>
2022-03-24 19:29:17 -0700

    more comments

--- Final_Project/compiler.py
+++ Final_Project/compiler.py
@@ -4,22 +4,27 @@ from clean_input import clean_input
 
 
 def main(argv):
+    # Make sure we have the correct number of arguments
     if len(argv) != 2:
         print("Usage: python compiler.py <filename>")
-        return 1
+        return
 
+    # Get the filename
     filename = argv[1]
 
+    # Read the file
     try:
         file = open(filename)
         lines = file.read().splitlines()
         file.close()
     except FileNotFoundError:
         print(f"File {filename} not found")
+        return
 
+    # Clean the input
     cleaned = clean_input(lines)
-    print(cleaned)
 
+    # Save the cleaned input
     with open(filename.split(".")[0] + "_cleaned.txt", "w") as file:
         file.write("\n".join([" ".join(line) for line in cleaned]))
 
